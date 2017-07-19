2f8c097fc767fb542b2370af8dafbb638f14a9c3
Andrewhanks <andrewhanks1113@gmail.com>
2017-07-19 11:25:28 +0800

    Modify codes for singleton pattern

--- app/src/main/java/com/example/andrewhanks/myapplication/creational/singleton/Singleton.java
+++ app/src/main/java/com/example/andrewhanks/myapplication/creational/singleton/Singleton.java
@@ -5,13 +5,15 @@ public class Singleton {
     private static Singleton instance = null;
 
     private Singleton() {
-        System.out.println("Singleton created");
+
     }
 
     synchronized static public Singleton getInstance() {
         if (instance == null) {
             instance = new Singleton();
+            System.out.println("Singleton created");
         }
+
         return instance;
     }
 }
